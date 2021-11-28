9dcc4e832c89a74db37350a6735423ac453f4329
madpeh <48161600+madpeh@users.noreply.github.com>
2021-11-28 16:47:30 +0100

    Add websockets

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import uvicorn
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, WebSocket
 from subprocess import PIPE, STDOUT
 import uuid
 import asyncio
@@ -16,27 +16,53 @@ async def post_simulations(num_agents: int):
     return {"id": simulation_id}
 
 
-@app.get("/simulations/{simulation_id}")
-async def get_simulation(simulation_id: str):
+# @app.get("/simulations/{simulation_id}")
+# async def get_simulation(simulation_id: str):
+#     if simulation_id not in simulations:
+#         raise HTTPException(status_code=404, detail="Simulation not found")
+#     max_lines_to_read = 50
+#     lines = []
+#     timeout = 2  # seconds
+#     while len(lines) < max_lines_to_read:
+#         try:
+#             line = await asyncio.wait_for(simulations[simulation_id].stdout.readline(), timeout)
+#         except:
+#             break
+#         if line:
+#             lines.append(line.decode("utf-8"))
+#     return {"output": lines}
+
+
+@app.websocket("/simulations/{simulation_id}")
+async def get_simulation(websocket: WebSocket, simulation_id: str):
+    await websocket.accept()
     if simulation_id not in simulations:
-        raise HTTPException(status_code=404, detail="Simulation not found")
-    max_lines_to_read = 50
-    lines = []
-    timeout = 2  # seconds
-    while len(lines) < max_lines_to_read:
+        websocket.close()
+    while True:
         try:
-            line = await asyncio.wait_for(simulations[simulation_id].stdout.readline(), timeout)
+            line = await simulations[simulation_id].stdout.readline()
+        except KeyError:
+            await websocket.send_json({"error": f"Simulation {simulation_id} not found"})
+            await websocket.close()
         except:
-            break
+            await websocket.send_json({"error": "Unknown error"})
+            await websocket.close()
         if line:
-            lines.append(line.decode("utf-8"))
-    return {"output": lines}
+            await websocket.send_json({"line": line.decode("utf-8")})
 
 
-@app.get("/simulations")
-async def get_simulations():
-    return {"running_simulations": list(simulations.keys())}
+# @app.get("/simulations")
+# async def get_simulations():
+#     return {"running_simulations": list(simulations.keys())}
 
+
+@app.websocket("/simulations")
+async def get_simulations(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        await websocket.send_json({"running_simulations": list(simulations.keys())})
+        await asyncio.sleep(2)
+        
 
 @app.delete("/simulations/{simulation_id}")
 async def delete_simulation(simulation_id: str):
