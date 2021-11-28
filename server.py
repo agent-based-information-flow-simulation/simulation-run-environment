1957b1dc6eaceb4c57622ecafe5e9fec2a8b470f
madpeh <48161600+madpeh@users.noreply.github.com>
2021-11-28 20:22:21 +0100

    Fix cors

--- server.py
+++ server.py
@@ -1,36 +1,23 @@
 import uvicorn
 from fastapi import FastAPI, HTTPException, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
 from subprocess import PIPE, STDOUT
 import uuid
 import asyncio
 
 app = FastAPI()
+cors_origins = ["http://localhost:3000"]
+cors_methods = ["*"]
+app.add_middleware(CORSMiddleware, allow_origins=cors_origins, allow_methods=cors_methods)
 simulations = {}
 
 
 @app.post("/simulations")
 async def post_simulations(num_agents: int):
-    simulation = await asyncio.create_subprocess_exec("python", "-u", "main.py", "-n", f"{num_agents}", "-d", f"{str(uuid.uuid4())}", stdout=PIPE, stderr=STDOUT)
     simulation_id = str(uuid.uuid4())
+    simulation = await asyncio.create_subprocess_exec("python", "-u", "main.py", "-n", f"{num_agents}", "-d", f"{simulation_id}", stdout=PIPE, stderr=STDOUT)
     simulations[simulation_id] = simulation
     return {"id": simulation_id}
-
-
-# @app.get("/simulations/{simulation_id}")
-# async def get_simulation(simulation_id: str):
-#     if simulation_id not in simulations:
-#         raise HTTPException(status_code=404, detail="Simulation not found")
-#     max_lines_to_read = 50
-#     lines = []
-#     timeout = 2  # seconds
-#     while len(lines) < max_lines_to_read:
-#         try:
-#             line = await asyncio.wait_for(simulations[simulation_id].stdout.readline(), timeout)
-#         except:
-#             break
-#         if line:
-#             lines.append(line.decode("utf-8"))
-#     return {"output": lines}
 
 
 @app.websocket("/simulations/{simulation_id}")
@@ -42,18 +29,14 @@ async def get_simulation(websocket: WebSocket, simulation_id: str):
         try:
             line = await simulations[simulation_id].stdout.readline()
         except KeyError:
-            await websocket.send_json({"error": f"Simulation {simulation_id} not found"})
+            await websocket.send_json({"id": simulation_id, "error": "Simulation not found"})
             await websocket.close()
         except:
-            await websocket.send_json({"error": "Unknown error"})
+            await websocket.send_json({"id": simulation_id, "error": "Unknown error"})
+            del simulations[simulation_id]
             await websocket.close()
         if line:
-            await websocket.send_json({"line": line.decode("utf-8")})
-
-
-# @app.get("/simulations")
-# async def get_simulations():
-#     return {"running_simulations": list(simulations.keys())}
+            await websocket.send_json({"id": simulation_id, "line": line.decode("utf-8").strip('\n')})
 
 
 @app.websocket("/simulations")
