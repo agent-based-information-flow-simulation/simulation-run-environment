44764f6d69b998b1c58525243e297cc8b8afda85
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-05 02:34:12 +0100

    Remove graph generator settings

--- translator/src/settings.py
+++ translator/src/settings.py
@@ -6,14 +6,8 @@ from pydantic import BaseSettings
 
 
 class AppSettings(BaseSettings):
-    client_cors_domain: str = os.environ.get("CLIENT_CORS_DOMAIN", "")
     enable_reload: bool = bool(os.environ.get("RELOAD", False))
     port: int = int(os.environ.get("PORT", 8000))
 
 
-class GraphGeneratorSettings(BaseSettings):
-    url: str = os.environ.get("GRAPH_GENERATOR_URL", "")
-
-
 app_settings = AppSettings()
-graph_generator_settings = GraphGeneratorSettings()
