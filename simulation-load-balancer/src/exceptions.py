e53a9314f60abbc55830e7df808bec66e0c4effa
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-05 17:37:02 +0100

    Add graph generator and translator exceptions

---
+++ simulation-load-balancer/src/exceptions.py
@@ -0,0 +1,13 @@
+class ExternalServiceException(Exception):
+    def __init__(self, status_code: int, body: str):
+        super().__init__("\n".join([f"[status {status_code}]", body]))
+
+
+class TranslatorException(ExternalServiceException):
+    def __init__(self, status_code: int, body: str):
+        super().__init__(status_code, body)
+
+
+class GraphGeneratorException(ExternalServiceException):
+    def __init__(self, status_code: int, body: str):
+        super().__init__(status_code, body)
