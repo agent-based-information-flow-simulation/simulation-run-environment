f1071b2136cde06331dfd79cc0bf0f276aa98c7a
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-05 02:28:16 +0100

    Remove simulation load balancer url

--- graph-generator/src/settings.py
+++ graph-generator/src/settings.py
@@ -10,14 +10,9 @@ class AppSettings(BaseSettings):
     port: int = int(os.environ.get("PORT", 8000))
 
 
-class SimulationLoadBalancerSettings(BaseSettings):
-    url: str = os.environ.get("SIMULATION_LOAD_BALANCER_URL", "")
-
-
 class CommunicationServerSettings(BaseSettings):
     domain: str = os.environ.get("COMMUNICATION_SERVER_DOMAIN", "")
 
 
 app_settings = AppSettings()
-simulation_load_balancer_settings = SimulationLoadBalancerSettings()
 communication_server_settings = CommunicationServerSettings()
