ff952ad41b8ee985d2c613c9e68b10800ba00b1f
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-07 02:37:41 +0100

    Change python version to 3.9

--- spade-instance/entrypoint.test.sh
+++ spade-instance/entrypoint.test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-/venv/bin/python3.10 -m coverage run \
+/venv/bin/python3.9 -m coverage run \
 --source=./src \
 --omit="./src/*__init__.py","./src/main.py" \
 -m pytest && \
-/venv/bin/python3.10 -m coverage report -m "$@"
+/venv/bin/python3.9 -m coverage report -m "$@"
