784f2c6a7e8b67c9c736c33d7c36d900136cd9f9
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-07 02:39:29 +0100

    Update settings

--- spade-instance/src/settings.py
+++ spade-instance/src/settings.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 
+import logging
 import os
 
 from pydantic import BaseSettings
+
+logging.basicConfig(format="%(levelname)s:\t  [%(name)s] %(message)s")
 
 
 class AppSettings(BaseSettings):
@@ -23,14 +26,25 @@ class CommunicationServerSettings(BaseSettings):
 
 class InstanceSettings(BaseSettings):
     id: str = os.environ.get("HOSTNAME")
+    status_url: str = os.environ.get("INSTANCE_STATUS_URL", "")
+
+
+class SimulationSettings(BaseSettings):
+    num_concurrent_registrations: int = int(
+        os.environ.get("NUM_CONCURRENT_REGISTRATION", 1)
+    )
 
 
 class SimulationLoadBalancerSettings(BaseSettings):
     url: str = os.environ.get("SIMULATION_LOAD_BALANCER_URL", "")
+    announcement_period: int = int(
+        os.environ.get("SIMULATION_LOAD_BALANCER_ANNOUNCEMENT_PERIOD", 10)
+    )
 
 
 app_settings = AppSettings()
 backup_settings = BackupSettings()
 communication_server_settings = CommunicationServerSettings()
 instance_settings = InstanceSettings()
+simulation_settings = SimulationSettings()
 simulation_load_balancer_settings = SimulationLoadBalancerSettings()
