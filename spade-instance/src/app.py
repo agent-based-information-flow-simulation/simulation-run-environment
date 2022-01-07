e34cd370fa28647b4347a1ed65b4bcde3e38b87d
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-07 02:38:01 +0100

    Add event handlers

--- spade-instance/src/app.py
+++ spade-instance/src/app.py
@@ -2,10 +2,13 @@ from __future__ import annotations
 
 from fastapi import FastAPI
 
+from src.handlers import load_balancer_status_handler, simulation_shutdown_handler
 from src.routers import router
 
 
 def get_app() -> FastAPI:
     app = FastAPI()
     app.include_router(router)
+    app.add_event_handler("startup", load_balancer_status_handler)
+    app.add_event_handler("shutdown", simulation_shutdown_handler)
     return app
---
+++ spade-instance/src/handlers.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+import logging
+import os
+from typing import Any, Coroutine
+
+import httpx
+from fastapi_utils.tasks import repeat_every
+
+from src.exceptions import SimulationException
+from src.settings import instance_settings, simulation_load_balancer_settings
+from src.state import state
+
+logger = logging.getLogger(__name__)
+logger.setLevel(level=os.environ.get("LOG_LEVEL_HANDLERS", "INFO"))
+
+
+def simulation_shutdown_handler() -> None:
+    try:
+        state.kill_simulation_process()
+    except SimulationException as e:
+        logger.info(str(e))
+
+
+@repeat_every(
+    seconds=simulation_load_balancer_settings.announcement_period,
+    raise_exceptions=True,
+    logger=logger,
+)
+async def load_balancer_status_handler() -> Coroutine[Any, Any, None]:
+    status, num_agents, num_alive_agents = state.get_state()
+    instance_status = {
+        "instance_id": instance_settings.id,
+        "status": status.name,
+        "num_agents": num_agents,
+        "num_alive_agents": num_alive_agents,
+    }
+    logger.info(f"Sending status to simulation load balancer: {instance_status}")
+    try:
+        async with httpx.AsyncClient() as client:
+            await client.post(simulation_load_balancer_settings.url, json=instance_status)
+    except Exception as e:
+        logger.warn(f"Error sending status to simulation load balancer: {e}")
