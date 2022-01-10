3b4c922ed1f8c8d2d8e58b691048d696572040a5
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-10 23:42:02 +0100

    Add exception

--- spade-instance/src/exceptions.py
+++ spade-instance/src/exceptions.py
@@ -2,10 +2,15 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
+if TYPE_CHECKING:  # pragma: no cover
     from src.status import Status
 
 
 class SimulationException(Exception):
     def __init__(self, status: Status, message: str):
         super().__init__(" ".join([f"[status {status}]", message]))
+
+
+class SimulationStateNotSetException(Exception):
+    def __init__(self):
+        super().__init__("Simulation state is not set")
