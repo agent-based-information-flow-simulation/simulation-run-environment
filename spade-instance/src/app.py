37628dbcbcb2271ca7bdcfec6d16c77cde1587b4
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-10 23:41:47 +0100

    Modify handlers

--- spade-instance/src/app.py
+++ spade-instance/src/app.py
@@ -3,19 +3,27 @@ from __future__ import annotations
 from fastapi import FastAPI
 
 from src.handlers import (
-    instance_state_handler,
-    simulation_process_health_check_handler,
-    simulation_shutdown_handler,
+    create_instance_state_handler,
+    create_simulation_process_health_check_handler,
+    create_simulation_state_shutdown_handler,
+    create_simulation_state_startup_handler,
 )
 from src.routers import router
 from src.settings import configure_logging
 
 
-def get_app() -> FastAPI:
+def get_app(unit_tests: bool = False) -> FastAPI:
     configure_logging()
+
     app = FastAPI()
     app.include_router(router)
-    app.add_event_handler("startup", instance_state_handler)
-    app.add_event_handler("startup", simulation_process_health_check_handler)
-    app.add_event_handler("shutdown", simulation_shutdown_handler)
+
+    if not unit_tests:  # pragma: no cover
+        app.add_event_handler("startup", create_simulation_state_startup_handler(app))
+        app.add_event_handler("startup", create_instance_state_handler(app))
+        app.add_event_handler(
+            "startup", create_simulation_process_health_check_handler(app)
+        )
+        app.add_event_handler("shutdown", create_simulation_state_shutdown_handler(app))
+
     return app
--- spade-instance/src/handlers.py
+++ spade-instance/src/handlers.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import logging
 import os
-from typing import Any, Coroutine
+from typing import TYPE_CHECKING, Any, Awaitable, Callable, Dict
 
 import httpx
 import psutil
@@ -10,31 +10,47 @@ from fastapi_utils.tasks import repeat_every
 
 from src.exceptions import SimulationException
 from src.settings import instance_settings, simulation_load_balancer_settings
-from src.state import state
+from src.state import State, get_app_simulation_state, set_app_simulation_state
+
+if TYPE_CHECKING:  # pragma: no cover
+    from fastapi import FastAPI
 
 logger = logging.getLogger(__name__)
 logger.setLevel(level=os.environ.get("LOG_LEVEL_HANDLERS", "INFO"))
 
 
-async def simulation_shutdown_handler() -> Coroutine[Any, Any, None]:
-    logger.info("Shutting down simulation")
-    try:
-        await state.kill_simulation_process()
-    except SimulationException as e:
-        logger.info(str(e))
-    logger.info("Simulation shutdown complete")
+def create_simulation_state_startup_handler(app: FastAPI) -> Callable[[], None]:
+    def simulation_state_startup_handler() -> None:
+        logger.info("Setting up simulation state")
+        set_app_simulation_state(app, State())
+        logger.info("Simulation state set up complete")
+
+    return simulation_state_startup_handler
 
 
-@repeat_every(
-    seconds=simulation_load_balancer_settings.announcement_period,
-    raise_exceptions=False,
-    logger=logger,
-)
-async def instance_state_handler() -> Coroutine[Any, Any, None]:
+def create_simulation_state_shutdown_handler(
+    app: FastAPI,
+) -> Callable[[], Awaitable[None]]:
+    async def simulation_state_shutdown_handler() -> Awaitable[None]:
+        logger.info("Shutting down simulation")
+        try:
+            await get_app_simulation_state(app).kill_simulation_process()
+        except SimulationException as e:
+            logger.info(str(e))
+        logger.info("Simulation shutdown complete")
+
+    return simulation_state_shutdown_handler
+
+
+async def get_instance_information(app: FastAPI) -> Awaitable[Dict[str, Any]]:
     api_memory_usage = psutil.Process().memory_info().rss / 1024 ** 2
-    simulation_memory_usage = await state.get_simulation_memory_usage()
-    status, simulation_id, num_agents, broken_agents = await state.get_state()
-    instance_state = {
+    simulation_memory_usage = await get_app_simulation_state(
+        app
+    ).get_simulation_memory_usage()
+    status, simulation_id, num_agents, broken_agents = await get_app_simulation_state(
+        app
+    ).get_state()
+    return {
         "status": status.name,
         "simulation_id": simulation_id,
         "num_agents": num_agents,
@@ -42,19 +58,38 @@ async def instance_state_handler() -> Coroutine[Any, Any, None]:
         "api_memory_usage_MiB": api_memory_usage,
         "simulation_memory_usage_MiB": simulation_memory_usage,
     }
-    url = f"{simulation_load_balancer_settings.url}/instances/{instance_settings.id}/state"
-    logger.info(f"Sending state to simulation load balancer ({url}): {instance_state}")
-    try:
-        async with httpx.AsyncClient() as client:
-            await client.put(url, json=instance_state)
-    except Exception as e:
-        logger.warn(f"Error while sending state to simulation load balancer: {e}")
 
 
-@repeat_every(
-    seconds=instance_settings.process_health_check_period,
-    raise_exceptions=False,
-    logger=logger,
-)
-async def simulation_process_health_check_handler() -> Coroutine[Any, Any, None]:
-    await state.verify_simulation_process()
+def create_instance_state_handler(app: FastAPI) -> Callable[[], Awaitable[None]]:
+    @repeat_every(
+        seconds=simulation_load_balancer_settings.announcement_period,
+        raise_exceptions=False,
+        logger=logger,
+    )
+    async def instance_state_handler() -> Awaitable[None]:
+        instance_state = await get_instance_information(app)
+        url = f"{simulation_load_balancer_settings.url}/instances/{instance_settings.id}/state"
+        logger.info(
+            f"Sending state to simulation load balancer ({url}): {instance_state}"
+        )
+        try:
+            async with httpx.AsyncClient() as client:
+                await client.put(url, json=instance_state)
+        except Exception as e:
+            logger.warn(f"Error while sending state to simulation load balancer: {e}")
+
+    return instance_state_handler
+
+
+def create_simulation_process_health_check_handler(
+    app: FastAPI,
+) -> Callable[[], Awaitable[None]]:
+    @repeat_every(
+        seconds=instance_settings.process_health_check_period,
+        raise_exceptions=False,
+        logger=logger,
+    )
+    async def simulation_process_health_check_handler() -> Awaitable[None]:
+        await get_app_simulation_state(app).verify_simulation_process()
+
+    return simulation_process_health_check_handler
