cb294cc2af9b39fd8b5faa1d67e65458b35c5765
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-10 23:39:24 +0100

    Split simulation script

---
+++ spade-instance/src/simulation/code_generation.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+import copy
+import datetime
+import logging
+import os
+import random
+from typing import TYPE_CHECKING, Any, Dict, List
+
+import httpx
+import numpy
+import orjson
+import spade
+
+from src.settings import backup_settings, communication_server_settings
+
+if TYPE_CHECKING:  # pragma: no cover
+    from spade.agent import Agent
+
+logger = logging.getLogger(__name__)
+logger.setLevel(level=os.environ.get("LOG_LEVEL_SIMULATION_CODE_GENERATION", "INFO"))
+
+
+def remove_imports(agent_code_lines: List[str]) -> List[str]:
+    return list(filter(lambda line: not line.startswith("import"), agent_code_lines))
+
+
+def generate_agents(
+    agent_code_lines: List[str], agent_data: List[Dict[str, Any]]
+) -> List[Agent]:
+    agent_logger = logging.getLogger("agent")
+    agent_logger.setLevel(level=os.environ.get("LOG_LEVEL_AGENT", "INFO"))
+
+    code_without_imports = remove_imports(agent_code_lines)
+
+    exec("\n".join(code_without_imports))
+
+    agents = []
+    for agent_data_dict in agent_data:
+        agent_type = agent_data_dict["type"]
+        del agent_data_dict["type"]
+        agent = locals()[agent_type](
+            password=communication_server_settings.password,
+            backup_url=backup_settings.agent_backup_url,
+            backup_period=backup_settings.period,
+            backup_delay=backup_settings.delay,
+            logger=agent_logger,
+            **agent_data_dict,
+        )
+        agents.append(agent)
+
+    logger.debug(f"Initialized agents: {agents}")
+
+    return agents
---
+++ spade-instance/src/simulation/initialization.py
@@ -0,0 +1,125 @@
+from __future__ import annotations
+
+import asyncio
+import logging
+import os
+from typing import TYPE_CHECKING, Any, Coroutine, Dict, List
+
+import aioxmpp
+from spade.behaviour import FSMBehaviour
+from spade.presence import PresenceManager
+
+from src.settings import simulation_settings
+
+if TYPE_CHECKING:  # pragma: no cover
+    from aioxmpp.structs import JID
+    from spade.agent import Agent
+    from spade.behaviour import CyclicBehaviour as Behaviour
+
+logger = logging.getLogger(__name__)
+logger.setLevel(level=os.environ.get("LOG_LEVEL_SIMULATION_INITIALIZATION", "INFO"))
+
+
+# https://github.com/agent-based-information-flow-simulation/spade/blob/6a857c2ae0a86b3bdfd20ccfcd28a11e1c6db81e/spade/agent.py#L171
+# TLS is set to false
+async def async_register(agent: Agent) -> Coroutine[Any, Any, None]:  # pragma: no cover
+    metadata = aioxmpp.make_security_layer(
+        None, no_verify=not agent.verify_security
+    )._replace(tls_required=False)
+    query = aioxmpp.ibr.Query(agent.jid.localpart, agent.password)
+    _, stream, _ = await aioxmpp.node.connect_xmlstream(
+        agent.jid, metadata, loop=agent.loop
+    )
+    await aioxmpp.ibr.register(stream, query)
+
+
+# https://github.com/agent-based-information-flow-simulation/spade/blob/6a857c2ae0a86b3bdfd20ccfcd28a11e1c6db81e/spade/agent.py#L93
+# TLS is set to false
+# register and connect client
+async def async_connect(agent: Agent) -> Coroutine[Any, Any, None]:  # pragma: no cover
+    await agent._hook_plugin_before_connection()
+    await async_register(agent)
+    agent.client = aioxmpp.PresenceManagedClient(
+        agent.jid,
+        aioxmpp.make_security_layer(
+            agent.password, no_verify=not agent.verify_security
+        )._replace(tls_required=False),
+        loop=agent.loop,
+        logger=logging.getLogger(agent.jid.localpart),
+    )
+    agent.message_dispatcher = agent.client.summon(
+        aioxmpp.dispatcher.SimpleMessageDispatcher
+    )
+    agent.presence = PresenceManager(agent)
+    await agent._async_connect()
+    agent.message_dispatcher.register_callback(
+        aioxmpp.MessageType.CHAT,
+        None,
+        agent._message_received,
+    )
+    await agent._hook_plugin_after_connection()
+
+
+async def connect_with_retry(
+    agent: Agent, retry_after: int
+) -> Coroutine[Any, Any, None]:
+    while True:
+        logger.debug(f"Attempting to connect agent {agent.jid}")
+        try:
+            await async_connect(agent)
+        except Exception as e:
+            logger.warning(
+                f"[{agent.jid}] Connection error (retry in {retry_after} seconds): {e}"
+            )
+            await asyncio.sleep(retry_after)
+            continue
+        break
+    logger.info(f"[{agent.jid}] Connected")
+
+
+async def connect_agents(agents: List[Agent]) -> Coroutine[Any, Any, None]:
+    concurrency = simulation_settings.registration_max_concurrency
+    semaphore = asyncio.Semaphore(concurrency)
+
+    logger.debug(f"Connecting agents with concurrency {concurrency}")
+
+    async def rate_limited_connect(agent: Agent) -> Coroutine[Any, Any, None]:
+        async with semaphore:
+            await connect_with_retry(
+                agent, simulation_settings.registration_retry_after
+            )
+
+    tasks = [asyncio.create_task(rate_limited_connect(agent)) for agent in agents]
+    await asyncio.gather(*tasks)
+
+
+# https://github.com/agent-based-information-flow-simulation/spade/blob/6a857c2ae0a86b3bdfd20ccfcd28a11e1c6db81e/spade/agent.py#L137
+# setup agent after it has been connected
+def setup_agent(agent: Agent) -> List[Behaviour]:
+    import copy
+
+    agent.setup()
+    agent._alive.set()
+    behaviours = copy.copy(agent.behaviours)
+
+    logger.debug(f"Agent {agent.jid} behaviours: {behaviours}")
+
+    for behaviour in agent.behaviours:  # pragma: no cover
+        if not behaviour.is_running:
+            behaviour.set_agent(agent)
+            if issubclass(type(behaviour), FSMBehaviour):
+                for _, state in behaviour.get_states().items():
+                    state.set_agent(agent)
+            behaviour.start()
+
+    return behaviours
+
+
+def setup_agents(agents: List[Agent]) -> Dict[JID, List[Behaviour]]:
+    agent_behaviours = {}
+    for agent in agents:
+        agent_behaviours[agent.jid] = setup_agent(agent)
+
+    logger.debug(f"Agent behaviours: {agent_behaviours}")
+
+    return agent_behaviours
