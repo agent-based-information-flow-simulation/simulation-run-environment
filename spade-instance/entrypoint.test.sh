306d7c42b6311243d37622eb03a9ef2767e1c87f
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-10 23:37:25 +0100

    Ignore deprecation warnings

--- spade-instance/entrypoint.test.sh
+++ spade-instance/entrypoint.test.sh
@@ -2,5 +2,5 @@
 /venv/bin/python3.9 -m coverage run \
 --source=./src \
 --omit="./src/*__init__.py","./src/main.py" \
--m pytest && \
+-m pytest -W ignore::DeprecationWarning && \
 /venv/bin/python3.9 -m coverage report -m "$@"
