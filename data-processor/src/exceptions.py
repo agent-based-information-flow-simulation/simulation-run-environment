069947d34637d8065ed7d352337313becbe0084f
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-13 16:30:49 +0100

    Format

--- data-processor/src/exceptions.py
+++ data-processor/src/exceptions.py
@@ -4,4 +4,3 @@ from __future__ import annotations
 class SimulationBackupAlreadyExistsException(Exception):
     def __init__(self, simulation_id: str):
         super().__init__(f"[simulation {simulation_id}] Backup already exists")
-
