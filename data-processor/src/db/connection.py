572e2fa99e50b9528c8ffd32ed397adaecad1b83
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-13 04:00:29 +0100

    Add connection handlers

--- data-processor/src/db/connection.py
+++ data-processor/src/db/connection.py
@@ -4,7 +4,8 @@ import logging
 import os
 from typing import TYPE_CHECKING, Any, Callable, Coroutine
 
-from motor.motor_asyncio import AsyncIOMotorClient
+from neo4j import AsyncGraphDatabase
+
 from src.settings import database_settings
 
 if TYPE_CHECKING:
@@ -16,19 +17,23 @@ logger.setLevel(level=os.environ.get("LOG_LEVEL_DB_CONNECTION", "INFO"))
 
 def create_startup_db_connection_handler(
     app: FastAPI,
-) -> Callable[[], Coroutine[Any, Any, None]]:
-    async def connect() -> None:
+) -> Callable[[], None]:
+    def connect() -> None:
         logger.info("Connecting to the database")
-        app.state.db_client = AsyncIOMotorClient(database_settings.url)
+        app.state.db_driver = AsyncGraphDatabase.driver(
+            database_settings.url, keep_alive=True, max_connection_pool_size=1000
+        )
         logger.info("Connected to the database")
 
     return connect
 
 
-def create_shutdown_db_connection_handler(app: FastAPI) -> Callable[[], None]:
-    def disconnect() -> None:
+def create_shutdown_db_connection_handler(
+    app: FastAPI,
+) -> Callable[[], Coroutine[Any, Any, None]]:
+    async def disconnect() -> None:
         logger.info("Disconnecting from the database")
-        app.state.db_client.close()
+        await app.state.db_driver.close()
         logger.info("Disconnected from the database")
 
     return disconnect
