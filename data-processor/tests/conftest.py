e80b9f6b0eb330f7c2bcb69a53e5f92480d4deff
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-13 04:00:53 +0100

    Format

--- data-processor/tests/conftest.py
+++ data-processor/tests/conftest.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING, List
 import pytest
 from asgi_lifespan import LifespanManager
 from httpx import AsyncClient
+
 from src.app import get_app
 from src.settings import app_settings
 
