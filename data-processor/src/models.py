3f5d1e98e7f87f390e3d6c4a8999e25db50db78f
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-13 03:59:53 +0100

    Add models

--- data-processor/src/models.py
+++ data-processor/src/models.py
@@ -1,5 +1,29 @@
 from __future__ import annotations
 
-from typing import List
+from typing import Dict, List, Union
 
+import orjson
 from pydantic import BaseModel
+
+
+class UpdateAgent(BaseModel):
+    jid: str
+    type: str
+    floats: Dict[str, float]
+    enums: Dict[str, str]
+    connections: Dict[str, List[str]]
+    messages: Dict[str, List[Dict[str, Union[float, str]]]]
+
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson.dumps
+
+
+class CreateAgent(BaseModel):
+    jid: str
+    type: str
+    connections: List[str]
+
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson.dumps
