790af6e798ee74cf3775ad58cb346bf06a7fe5c2
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-16 21:51:34 +0100

    Delete kafka consumer

--- kafka-consumer/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-/api/startup_scripts/wait_for_db.sh && \
-/api/startup_scripts/wait_for_kafka.sh && \
-/venv/bin/python3.9 -m src.main
--- kafka-consumer/entrypoint.test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-/venv/bin/python3.9 -m coverage run \
---source=./src \
---omit="./src/*__init__.py","./src/main.py" \
--m pytest && \
-/venv/bin/python3.9 -m coverage report -m "$@"
