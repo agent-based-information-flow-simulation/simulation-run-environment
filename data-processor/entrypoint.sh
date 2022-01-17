1b069f04b9446d9bef7bf834dd2b3874de2bea77
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-17 02:29:38 +0100

    Update register in proxy script

--- data-processor/entrypoint.sh
+++ data-processor/entrypoint.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+# env variables:
+# - PROXY_REGISTRATION_ADDRESS
+# - PROXY_REGISTRATION_BACKEND_DATA_PROCESSOR_NAME
+# - PROXY_REGISTRATION_BACKEND_DATA_PROCESSOR_PORT
+# - PROXY_REGISTRATION_MAX_RETRIES
+# - PROXY_REGISTRATION_USER_NAME
+# - PROXY_REGISTRATION_USER_PASSWORD
+
 /api/startup_scripts/wait_for_db.sh && \
 /api/startup_scripts/wait_for_proxy.sh && \
-/venv/bin/python3.9 -u /api/startup_scripts/register_in_proxy.py data-processor-proxy:5555 `cat /etc/hostname` data_processor 8000 && \
+/venv/bin/python3.9 -u /api/startup_scripts/register_in_proxy.py \
+    "${PROXY_REGISTRATION_ADDRESS}" \
+    "${PROXY_REGISTRATION_USER_NAME}" \
+    "${PROXY_REGISTRATION_USER_PASSWORD}" \
+    "${PROXY_REGISTRATION_MAX_RETRIES}" \
+    `cat /etc/hostname` \
+    "${PROXY_REGISTRATION_BACKEND_DATA_PROCESSOR_NAME}" \
+    "${PROXY_REGISTRATION_BACKEND_DATA_PROCESSOR_PORT}" && \
 /venv/bin/python3.9 -m src.main
--- data-processor/startup_scripts/register_in_proxy.py
+++ data-processor/startup_scripts/register_in_proxy.py
@@ -7,12 +7,12 @@ import requests as r
 PROXY_HOSTNAME_WITH_PORT = sys.argv[1]
 URL = f"http://{PROXY_HOSTNAME_WITH_PORT}/v2"
 GET_VERSION_URL = f"{URL}/services/haproxy/configuration/backends"
-ENDPOINT_USER = "admin"
-ENDPOINT_PASSWORD = "admin"
-NUM_MAX_RETRIES = 100
-IMAGE_HOSTNAME = sys.argv[2]
-BACKEND_NAME = sys.argv[3]
-BACKEND_PORT = int(sys.argv[4])
+ENDPOINT_USER = sys.argv[2]
+ENDPOINT_PASSWORD = sys.argv[3]
+NUM_MAX_RETRIES = int(sys.argv[4])
+IMAGE_HOSTNAME = sys.argv[5]
+BACKEND_NAME = sys.argv[6]
+BACKEND_PORT = int(sys.argv[7])
 
 
 def register(backend_name, port):
@@ -37,7 +37,9 @@ def register(backend_name, port):
             },
         )
         if response.status_code == 202:
-            print(f"Server {server_name} added to haproxy")
+            print(
+                f"Server {server_name} (port {port}) added to haproxy backend {backend_name}"
+            )
             return
         elif (
             f"Server {server_name} already exists in backend"
