077befb5de293fb835d0072e88e733ab39ee8fd2
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-17 01:48:04 +0100

    Add tests

--- spade-instance/tests/test_src/test_dependencies.py
+++ spade-instance/tests/test_src/test_dependencies.py
@@ -4,9 +4,11 @@ from typing import TYPE_CHECKING
 from unittest.mock import Mock
 
 import pytest
+from aiokafka import AIOKafkaProducer
 from starlette.requests import Request
 
-from src.dependencies import create_get_simulation_state
+from src.dependencies import create_get_kafka, create_get_simulation_state
+from src.kafka import set_app_kafka
 from src.state import State, set_app_simulation_state
 
 if TYPE_CHECKING:
@@ -26,3 +28,15 @@ def test_get_simulation_state_returns_simulation_state(app: FastAPI) -> None:
     state = get_simulation_state(mocked_request)
 
     assert state == mocked_state
+
+
+def test_get_kafka_returns_kafka(app: FastAPI) -> None:
+    mocked_kafka = Mock(spec=AIOKafkaProducer)
+    set_app_kafka(app, mocked_kafka)
+    mocked_request = Mock(spec=Request)
+    mocked_request.app = app
+    get_kafka = create_get_kafka()
+
+    kafka = get_kafka(mocked_request)
+
+    assert kafka == mocked_kafka
