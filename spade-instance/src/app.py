7636b90a34cd178a3cd26f0ca498a7e4905f037c
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-17 01:45:15 +0100

    Fix imports

--- spade-instance/src/app.py
+++ spade-instance/src/app.py
@@ -2,14 +2,20 @@ from __future__ import annotations
 
 from fastapi import FastAPI
 
-from src.handlers import (
+from src.kafka import (
+    create_shutdown_kafka_connection_handler,
+    create_startup_kafka_connection_handler,
+)
+from src.repeated_tasks import (
     create_instance_state_handler,
     create_simulation_process_health_check_handler,
-    create_simulation_state_shutdown_handler,
-    create_simulation_state_startup_handler,
 )
 from src.routers import router
 from src.settings import configure_logging
+from src.state import (
+    create_simulation_state_shutdown_handler,
+    create_simulation_state_startup_handler,
+)
 
 
 def get_app(unit_tests: bool = False) -> FastAPI:
@@ -24,6 +30,8 @@ def get_app(unit_tests: bool = False) -> FastAPI:
         app.add_event_handler(
             "startup", create_simulation_process_health_check_handler(app)
         )
+        app.add_event_handler("startup", create_startup_kafka_connection_handler(app))
         app.add_event_handler("shutdown", create_simulation_state_shutdown_handler(app))
+        app.add_event_handler("shutdown", create_shutdown_kafka_connection_handler(app))
 
     return app
