0412c9e26d7feb3938ed7b2cb7653ae817c81c40
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-17 01:37:54 +0100

    Update wait for db script

--- data-processor/startup_scripts/wait_for_db.sh
+++ data-processor/startup_scripts/wait_for_db.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-while ! nc -z db 7474; do sleep 1; done;
-echo "db is up"
+# env variables:
+# - WAIT_FOR_DB_ADDRESS
+
+IFS=':' read -ra ADDRESS_SPLIT <<< "${WAIT_FOR_DB_ADDRESS}"
+if [ "${#ADDRESS_SPLIT[@]}" -ne "2" ]; then
+    echo "Expected argument format: HOST:PORT"
+    echo "Unexpected argument: ${ADDRESS}"
+    echo "Split (on ':' delimiter) into:"
+    for PART in "${ADDRESS_SPLIT[@]}"; do
+        echo "${PART}"
+    done
+    exit 1
+fi
+HOST="${ADDRESS_SPLIT[0]}"
+PORT="${ADDRESS_SPLIT[1]}"
+
+while ! nc -z "${HOST}" "${PORT}"; do sleep 1; done;
+echo "Database is up"
