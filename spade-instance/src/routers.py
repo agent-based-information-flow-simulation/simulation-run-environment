40c014d89a99b8130a9f231a99cf7529cc377213
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-17 01:47:10 +0100

    Add streaming data to kafka

--- spade-instance/src/routers.py
+++ spade-instance/src/routers.py
@@ -4,14 +4,14 @@ import logging
 import os
 from typing import Any, Dict
 
-import httpx
+from aiokafka import AIOKafkaProducer
 from fastapi import APIRouter, Depends
 from fastapi.exceptions import HTTPException
 
-from src.dependencies import state
+from src.dependencies import kafka, state
 from src.exceptions import SimulationException
 from src.models import CreateSimulation, DeletedSimulation, InstanceStatus
-from src.settings import backup_settings
+from src.settings import kafka_settings
 from src.state import State
 
 logger = logging.getLogger(__name__)
@@ -49,11 +49,14 @@ async def delete_simulation(state: State = Depends(state)):
 
 
 @router.post("/internal/simulation/agent_data", status_code=201)
-async def backup_agent_data(body: Dict[Any, Any], state: State = Depends(state)):
+async def backup_agent_data(
+    body: Dict[str, Any],
+    state: State = Depends(state),
+    kafka: AIOKafkaProducer = Depends(kafka),
+):
     logger.debug(f"Backup from agent: {body['jid']}")
-    url = f"{backup_settings.api_backup_url}/simulations/{await state.get_simulation_id()}/backup/agents"
-    async with httpx.AsyncClient() as client:
-        await client.put(url, json=body)
+    body["simulation_id"] = await state.get_simulation_id()
+    await kafka.send(topic=kafka_settings.topic, key=body["jid"], value=body)
 
 
 @router.post("/internal/instance/status", status_code=201)
