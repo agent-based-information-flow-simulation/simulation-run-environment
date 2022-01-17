7c83d946b1dfa450ba8f481e8b2f89dbc40687f5
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-17 01:45:26 +0100

    Add kafka dependency

--- spade-instance/src/dependencies.py
+++ spade-instance/src/dependencies.py
@@ -2,8 +2,10 @@ from __future__ import annotations
 
 from typing import Callable
 
+from aiokafka import AIOKafkaProducer
 from starlette.requests import Request
 
+from src.kafka import get_app_kafka
 from src.state import State, get_app_simulation_state
 
 
@@ -14,4 +16,12 @@ def create_get_simulation_state() -> Callable[[Request], State]:
     return get_simulation_state
 
 
+def create_get_kafka() -> Callable[[Request], AIOKafkaProducer]:
+    def get_kafka(request: Request) -> AIOKafkaProducer:
+        return get_app_kafka(request.app)
+
+    return get_kafka
+
+
 state: Callable[[Request], State] = create_get_simulation_state()
+kafka: Callable[[Request], AIOKafkaProducer] = create_get_kafka()
