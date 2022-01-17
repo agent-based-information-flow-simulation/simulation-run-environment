7ba4759fe5c448259df1de18c188e6cb9c50bf46
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-17 01:36:21 +0100

    Add and operator

--- data-processor/entrypoint.sh
+++ data-processor/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/api/startup_scripts/wait_for_db.sh
-/api/startup_scripts/wait_for_proxy.sh
+/api/startup_scripts/wait_for_db.sh && \
+/api/startup_scripts/wait_for_proxy.sh && \
 /venv/bin/python3.9 -u /api/startup_scripts/register_in_proxy.py data-processor-proxy:5555 `cat /etc/hostname` data_processor 8000 && \
 /venv/bin/python3.9 -m src.main
