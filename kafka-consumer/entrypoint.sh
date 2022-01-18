1c9f329d0cb4d17aad0216fc271221ea4faa3017
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-18 03:58:32 +0100

    Rename workdir to app

--- kafka-consumer/entrypoint.sh
+++ kafka-consumer/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-/api/startup_scripts/wait_for_db.sh && \
-/api/startup_scripts/wait_for_kafka.sh && \
-/api/startup_scripts/wait_for_kafka_topics.sh && \
+/app/startup_scripts/wait_for_db.sh && \
+/app/startup_scripts/wait_for_kafka.sh && \
+/app/startup_scripts/wait_for_kafka_topics.sh && \
 /venv/bin/python3.9 -m src.main
