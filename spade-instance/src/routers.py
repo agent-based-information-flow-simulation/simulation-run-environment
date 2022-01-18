5ade0f04bf543a0ad350a7cd3b308518910dbfc1
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-18 21:44:29 +0100

    Parse agent data with orjson

--- spade-instance/src/routers.py
+++ spade-instance/src/routers.py
@@ -4,9 +4,11 @@ import logging
 import os
 from typing import Any, Dict
 
+import orjson
 from aiokafka import AIOKafkaProducer
 from fastapi import APIRouter, Depends
 from fastapi.exceptions import HTTPException
+from starlette.requests import Request
 
 from src.dependencies import kafka, state
 from src.exceptions import SimulationException
@@ -50,10 +52,11 @@ async def delete_simulation(state: State = Depends(state)):
 
 @router.post("/internal/simulation/agent_data", status_code=201)
 async def backup_agent_data(
-    body: Dict[str, Any],
+    request: Request,
     state: State = Depends(state),
     kafka: AIOKafkaProducer = Depends(kafka),
 ):
+    body: Dict[str, Any] = orjson.loads(await request.body())
     logger.debug(f"Backup from agent: {body['jid']}")
     body["simulation_id"] = await state.get_simulation_id()
     await kafka.send(topic=kafka_settings.topic, key=body["jid"], value=body)
