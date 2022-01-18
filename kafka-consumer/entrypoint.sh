089b6255c6ffb3ae4b0bd46573e0fbeef34e162d
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-18 03:14:59 +0100

    Add waiting for kafka topics

--- kafka-consumer/entrypoint.sh
+++ kafka-consumer/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 /api/startup_scripts/wait_for_db.sh && \
 /api/startup_scripts/wait_for_kafka.sh && \
+/api/startup_scripts/wait_for_kafka_topics.sh && \
 /venv/bin/python3.9 -m src.main
