dd6d3c7025a225566abfbae37d263df717d141ae
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-22 18:21:51 +0100

    Format

--- data-processor/src/app.py
+++ data-processor/src/app.py
@@ -12,7 +12,7 @@ from src.settings import configure_logging
 
 def get_app(unit_tests: bool = False) -> FastAPI:
     configure_logging()
-    
+
     app = FastAPI()
     app.include_router(router)
 
