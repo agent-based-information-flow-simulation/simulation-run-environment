91171765b0c574b161c84590806892bd9eb13a99
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-22 21:48:02 +0100

    Add retreiving agent type list length

--- data-processor/src/db/repositories/statistics.py
+++ data-processor/src/db/repositories/statistics.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 
 from src.db.repositories.base import BaseRepository
 
@@ -40,16 +40,36 @@ class StatisticsRepository(BaseRepository):
     async def get_agent_type_property(
         self, simulation_id: str, agent_type: str, property_: str
     ) -> Record | None:
-        get_agent_type_property = """
+        get_agent_type_property_query = """
         MATCH (agent:Agent {simulation_id: $simulation_id, type: $agent_type})
         RETURN COLLECT(agent[$property]) AS property
         """
 
         records = await self.session.run(
-            get_agent_type_property,
+            get_agent_type_property_query,
             simulation_id=simulation_id,
             agent_type=agent_type,
             property=property_,
         )
 
         return await records.single()
+
+    async def get_agent_type_list_length(
+        self, simulation_id: str, agent_type: str, list_name: str
+    ) -> List[Record]:
+        get_agent_type_list_length_query = """
+        MATCH (agent:Agent {simulation_id: $simulation_id, type: $agent_type})
+        OPTIONAL MATCH (agent)-[relationship]->() WHERE TYPE(relationship) = $relationship_name
+        WITH agent, COUNT(relationship) AS relationship_count
+        RETURN relationship_count
+        ORDER BY relationship_count
+        """
+
+        records = await self.session.run(
+            get_agent_type_list_length_query,
+            simulation_id=simulation_id,
+            agent_type=agent_type,
+            relationship_name=list_name,
+        )
+
+        return [record async for record in records]
