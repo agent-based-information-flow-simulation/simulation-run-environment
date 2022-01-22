06a140c61a79f7fcc1b225b6e9ebea0a20a4fad1
madpeh <48161600+madpeh@users.noreply.github.com>
2022-01-23 00:49:18 +0100

    Add statistics validator

--- data-processor/src/models.py
+++ data-processor/src/models.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import List
 
 import orjson
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class CreateAgent(BaseModel):
@@ -17,8 +17,14 @@ class CreateAgent(BaseModel):
 
 
 class Statistics(BaseModel):
-    labels: List[str]
     data: List[float]
+    labels: List[str]
+
+    @validator("labels")
+    def labels_and_data_must_have_the_same_length(cls, value, values):
+        if len(value) != len(values["data"]):
+            raise ValueError("labels and data must have the same length")
+        return value
 
     class Config:
         json_loads = orjson.loads
