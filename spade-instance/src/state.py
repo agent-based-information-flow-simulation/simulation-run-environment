5cf24dc1512be012fa315801091636a05496b359
madpeh <48161600+madpeh@users.noreply.github.com>
2023-05-22 17:47:43 +0200

    Add async queues for backups

--- spade-instance/src/state.py
+++ spade-instance/src/state.py
@@ -7,12 +7,15 @@ from multiprocessing import Process
 from typing import TYPE_CHECKING, Any, Callable, Coroutine, Dict, List, Tuple
 
 import psutil
+from aioprocessing import AioQueue
 
 from src.exceptions import (
     SimulationException,
     SimulationIdNotSetException,
     SimulationStateNotSetException,
 )
+from src.kafka import get_app_kafka
+from src.settings import kafka_settings
 from src.simulation.main import main
 from src.status import Status
 
@@ -26,19 +29,38 @@ logger.setLevel(level=os.environ.get("LOG_LEVEL_STATE", "INFO"))
 
 
 class State:
-    def __init__(self):
+    def __init__(self, app: FastAPI):
+        self.app: FastAPI = app
         self.mutex: Lock = asyncio.Lock()
         self.status: Status = Status.IDLE
         self.simulation_process: Process | None = None
         self.simulation_id: str | None = None
         self.num_agents: int = 0
         self.broken_agents: List[str] = []
+        self.agent_updates: AioQueue | None = None
+        self.simulation_status_update: AioQueue | None = None
 
-    def _clean_state(self) -> None:
+    async def _clean_state(self) -> None:
+        # not the most elegant solution
+        # it waits for some time for the queues to be empty after the simulation is killed
+        # and then it puts poison pills into the queues
+        # it's been noticed that sometimes the poison pills are not processed if put immediately
+        # TODO: find a better solution
+        await self.await_empty_queue(
+            self.simulation_status_updates, wait_before_seconds=0.5, every_seconds=0.5
+        )
+        await self.await_empty_queue(
+            self.agent_updates, wait_before_seconds=0.5, every_seconds=0.5
+        )
+        await self.simulation_status_updates.coro_put(None)
+        await self.agent_updates.coro_put(None)
+
         self.simulation_process = None
         self.simulation_id = None
         self.num_agents = 0
         self.broken_agents = []
+        self.agent_updates = None
+        self.simulation_status_updates = None
 
     async def update_active_state(
         self, status: Status, num_agents: int, broken_agents: List[str]
@@ -87,10 +109,93 @@ class State:
 
             self.status = Status.STARTING
             self.simulation_id = simulation_id
+            self.agent_updates = AioQueue()
+            self.simulation_status_updates = AioQueue()
             self.simulation_process = Process(
-                target=main, args=(agent_code_lines, agent_data)
+                target=main,
+                args=(
+                    agent_code_lines,
+                    agent_data,
+                    self.agent_updates,
+                    self.simulation_status_updates,
+                ),
             )
             self.simulation_process.start()
+            asyncio.create_task(
+                self.read_and_save_agent_updates(self.agent_updates, self.simulation_id)
+            )
+            asyncio.create_task(
+                self.read_and_save_simulation_status_updates(
+                    self.simulation_status_updates, self.simulation_id
+                )
+            )
+
+    async def read_and_save_agent_updates(
+        self, queue: AioQueue, simulation_id: str
+    ) -> None:
+        queue_name = f"agent updates ({simulation_id})"
+        queue_size_task = asyncio.create_task(
+            self.show_queue_size(queue, every_seconds=30, name=queue_name)
+        )
+        kafka = get_app_kafka(self.app)
+        logger.info(f"Started reading {queue_name} for simulation {simulation_id}")
+        while True:
+            update: Dict[str, Any] | None = await queue.coro_get()
+            if update is None:
+                break
+
+            update["simulation_id"] = simulation_id
+            await kafka.send(
+                topic=kafka_settings.topic, key=update["jid"], value=update
+            )
+
+        queue_size_task.cancel()
+        logger.info(f"Stopped reading {queue_name} for simulation {simulation_id}")
+        logger.info(
+            f"Unread items in {queue_name} queue after stopping: {queue.qsize()}"
+        )
+
+    # TODO: reuse the code from 'read_and_save_agent_updates'
+    async def read_and_save_simulation_status_updates(
+        self, queue: AioQueue, simulation_id: str
+    ) -> None:
+        queue_name = f"simulation status updates ({simulation_id})"
+        queue_size_task = asyncio.create_task(
+            self.show_queue_size(queue, every_seconds=30, name=queue_name)
+        )
+        logger.info(f"Started reading {queue_name} for simulation {simulation_id}")
+        while True:
+            update: Dict[str, Any] | None = await queue.coro_get()
+            if update is None:
+                break
+
+            await self.update_active_state(
+                status=update["status"],
+                num_agents=update["num_agents"],
+                broken_agents=update["broken_agents"],
+            )
+
+        queue_size_task.cancel()
+        logger.info(f"Stopped reading {queue_name} for simulation {simulation_id}")
+        logger.info(
+            f"Unread items in {queue_name} queue after stopping: {queue.qsize()}"
+        )
+
+    async def show_queue_size(
+        self, queue: AioQueue, every_seconds: float, name: str
+    ) -> None:
+        while True:
+            logger.info(f"Unread items in {name} queue: {queue.qsize()}")
+            await asyncio.sleep(every_seconds)
+
+    async def await_empty_queue(
+        self, queue: AioQueue, wait_before_seconds: float, every_seconds: float
+    ) -> None:
+        await asyncio.sleep(wait_before_seconds)
+        while True:
+            if queue.empty():
+                break
+            await asyncio.sleep(every_seconds)
 
     async def kill_simulation_process(self) -> Coroutine[Any, Any, None]:
         logger.debug(f"Killing simulation, state: {await self.get_state()}")
@@ -100,7 +205,7 @@ class State:
 
             self.status = Status.IDLE
             self.simulation_process.kill()
-            self._clean_state()
+            await self._clean_state()
 
     async def get_simulation_memory_usage(self) -> Coroutine[Any, Any, float]:
         logger.debug(
@@ -113,7 +218,7 @@ class State:
             ):
                 return (
                     psutil.Process(self.simulation_process.pid).memory_info().rss
-                    / 1024 ** 2
+                    / 1024**2
                 )
 
             return 0.0
@@ -127,7 +232,7 @@ class State:
             ):
                 logger.warning("Simulation process is dead")
                 self.status = Status.DEAD
-                self._clean_state()
+                await self._clean_state()
 
 
 def set_app_simulation_state(app: FastAPI, state: State) -> None:
@@ -144,7 +249,7 @@ def get_app_simulation_state(app: FastAPI) -> State:
 def create_simulation_state_startup_handler(app: FastAPI) -> Callable[[], None]:
     def simulation_state_startup_handler() -> None:
         logger.info("Setting up simulation state")
-        set_app_simulation_state(app, State())
+        set_app_simulation_state(app, State(app))
         logger.info("Simulation state set up complete")
 
     return simulation_state_startup_handler
