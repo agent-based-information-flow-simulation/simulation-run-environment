490e2ae10c9113ae322db05f84bff174057d1e43
Kajetan Rachwał <krachwal@ibspan.waw.pl>
2023-07-20 14:32:19 +0200

    Add module generation to the system

--- translator/src/models.py
+++ translator/src/models.py
@@ -7,8 +7,11 @@ from pydantic import BaseModel
 
 class AgentsAssemblyCode(BaseModel):
     code_lines: List[str]
+    module_lines: List[List[str]]
+
 
 
 class PythonSpadeCode(BaseModel):
     agent_code_lines: List[str]
     graph_code_lines: List[str]
+    module_code_lines: List[str]
--- translator/src/routers.py
+++ translator/src/routers.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from aasm import __version__, get_spade_code
+from aasm import __version__, get_spade_code, get_modules_for_target
 from fastapi import APIRouter
 
 from src.models import AgentsAssemblyCode, PythonSpadeCode
@@ -10,8 +10,10 @@ router = APIRouter()
 
 @router.post("/python/spade", response_model=PythonSpadeCode, status_code=200)
 async def translate_aasm(agent_assembly_code: AgentsAssemblyCode):
-    spade_code = get_spade_code(agent_assembly_code.code_lines)
+    modules = get_modules_for_target(agent_assembly_code.module_lines, "spade")
+    spade_code = get_spade_code(agent_assembly_code.code_lines, modules=modules)
     return PythonSpadeCode(
         agent_code_lines=spade_code.agent_code_lines,
         graph_code_lines=spade_code.graph_code_lines,
+        module_code_lines=spade_code.module_code_lines,
     )
