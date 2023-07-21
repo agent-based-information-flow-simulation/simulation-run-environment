0798b024e313f5a7bcd983e803fdffdf03c926b8
Kajetan Rachwał <krachwal@ibspan.waw.pl>
2023-07-21 13:05:44 +0200

    Add flush redis on SLB startup

--- simulation-load-balancer/src/handlers.py
+++ simulation-load-balancer/src/handlers.py
@@ -18,14 +18,19 @@ logger.setLevel(level=os.environ.get("LOG_LEVEL_HANDLERS", "INFO"))
 def create_startup_redis_connection_handler(
     app: FastAPI,
 ) -> Callable[[], Awaitable[None]]:
-    async def connect_redis() -> Coroutine[Any, Any, None]:
-        logger.info("Connecting to redis")
+    async def connect_redis():
+        logger.info("Connecting to redis...")
         app.state.redis = await aioredis.Redis(
             host=redis_settings.redis_address,
             port=redis_settings.redis_port,
             password=redis_settings.redis_password,
         )
         logger.info("Connected to redis")
+        logger.info("Flushing all keys...")
+        for key in await app.state.redis.keys("*"):
+            await app.state.redis.delete(key)
+        logger.info("Flushed all keys")
+        logger.info("Setting up redis complete")
 
     return connect_redis
 
@@ -33,7 +38,7 @@ def create_startup_redis_connection_handler(
 def create_shutdown_redis_connection_handler(
     app: FastAPI,
 ) -> Callable[[], Awaitable[None]]:
-    async def disconnect_redis() -> Coroutine[Any, Any, None]:
+    async def disconnect_redis():
         logger.info("Disconnecting from redis")
         await app.state.redis.close()
         logger.info("Disconnected from redis")
